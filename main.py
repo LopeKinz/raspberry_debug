fe89cf858e8641143c88c57a7032a2b894dbc85e
LopeKinz <48872005+LopeKinz@users.noreply.github.com>
2023-01-06 07:13:34 +0000

    a

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import psutil
+import subprocess
 
 def get_system_info():
     cpu_usage = psutil.cpu_percent()
@@ -7,6 +8,14 @@ def get_system_info():
     disk_usage = psutil.disk_usage('/')
     info = f'CPU Usage: {cpu_usage}%\nVirtual Memory: {virtual_memory.used / (1024 ** 3):.2f} GB / {virtual_memory.total / (1024 ** 3):.2f} GB ({virtual_memory.percent}%)\nDisk Usage: {disk_usage.used / (1024 ** 3):.2f} GB / {disk_usage.total / (1024 ** 3):.2f} GB ({disk_usage.percent}%)'
     return info
+
+
+
+try:
+    output = subprocess.check_output(['sudo', 'iwgetid'])
+    print("Connected Wifi SSID: " + output.split('"')[1])
+except Exception as e:
+    print("Not connected to any Wifi")
 
 root = tk.Tk()
 root.title('System Info')
